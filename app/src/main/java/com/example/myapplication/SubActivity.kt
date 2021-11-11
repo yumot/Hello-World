481454a136bda2466ef511708c3487f7ab7a6a0c
yumoto <yumo.tennis.20@outlook.jp>
2021-11-11 21:30:42 +0900

    ボタン押下したときに画面遷移

--- app/src/main/java/com/example/myapplication/SubActivity.kt
+++ app/src/main/java/com/example/myapplication/SubActivity.kt
@@ -1,19 +1,18 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SubActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sub)
 
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
-            val intent: Intent = Intent(this, SubActivity::class.java)
-            startActivity(intent)
+            finish()
         }
     }
 }
